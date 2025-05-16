7b93d9a8ba3a1de5cf51694862462173c88dce3b
Bansi297 <bansarivora297@gmail.com>
2025-05-16 17:01:34 -0400

    Update complainHandle.js

--- frontend/src/redux/userRelated/complainHandle.js
+++ frontend/src/redux/userRelated/complainHandle.js
@@ -5,7 +5,7 @@ import {
     getFailed,
     getError
 } from './complainSlice';
-const REACT_APP_BASE_URL = "http://localhost:6000";
+const REACT_APP_BASE_URL = "https://school-admision-mern-backend.onrender.com";
 
 export const getAllComplains = (id, address) => async (dispatch) => {
     dispatch(getRequest());
