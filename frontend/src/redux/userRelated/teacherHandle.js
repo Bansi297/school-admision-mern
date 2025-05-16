ad218047a5725b27353659b096beac1a73485fb3
Bansi297 <bansarivora297@gmail.com>
2025-05-16 17:03:48 -0400

    Update teacherHandle.js

--- frontend/src/redux/userRelated/teacherHandle.js
+++ frontend/src/redux/userRelated/teacherHandle.js
@@ -7,7 +7,7 @@ import {
     postDone,
     doneSuccess
 } from './teacherSlice';
-const REACT_APP_BASE_URL = "http://localhost:6000";
+const REACT_APP_BASE_URL = "https://school-admision-mern-backend.onrender.com";
 
 export const getAllTeachers = (id) => async (dispatch) => {
     dispatch(getRequest());
